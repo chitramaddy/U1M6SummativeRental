4d89e50aedc1e0e7b8b4a3fd6cadc18001707bea
Your Name <chitramaddy@gmail.com>
2019-07-08 16:00:44 -0400

    customer controller

---
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/controller/CustomerController.java
@@ -0,0 +1,73 @@
+package com.company.U1M6Summative.controller;
+
+import com.company.U1M6Summative.model.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private List<Customer> customerList = new ArrayList<Customer>();
+
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public Customer addCustomer(@RequestBody Customer customer){
+
+        int id = 1;
+        customer.setCustomerId(id);
+        customerList.add(customer);
+        return customer;
+
+    }
+
+    @RequestMapping(value = "/customer", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Customer getCustomer(@PathVariable int id){
+
+        id = 1;
+        Customer customer = new Customer();
+        customer.setFirstName("Chitra");
+        customer.setLastName("Madhan");
+        customer.setEmail("cm@cm.com");
+        customer.setPhone("123-456-7890");
+        customer.setCompany("north");
+        customer.setCustomerId(id);
+
+        return customer;
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCustomer(@PathVariable int id){
+
+           for(Customer customer : customerList) {
+            if (customer.getCustomerId() == id) {
+
+                customerList.remove(customer);
+
+            }
+    }
+
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Customer getCustomerById(@PathVariable int id) {
+
+        for(Customer customer : customerList) {
+            if (customer.getCustomerId() == id)
+                return customer;
+        }
+
+        throw new IllegalArgumentException("customer not found.");
+    }
+}
--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
@@ -1,9 +1,14 @@
 package com.company.U1M6Summative.dao;
 
 import com.company.U1M6Summative.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -23,35 +28,66 @@ public class CustomerDaoJdbcTemplateImpl implements CustomerDao{
     private static final String UPDATE_CUSOTMER_SQL =
             "update customer set first_name = ?, last_name = ?, email = ?, company = ?, phone = ? where customer_id = ?";
 
-    private static final String DELETE_ALBUM =
+    private static final String DELETE_CUSTOMER_SQL =
             "delete from customer where customer_id = ?";
 
-
+    @Autowired
+    public CustomerDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
+    @Transactional
     public Customer addCusotmer(Customer customer) {
+        jdbcTemplate.update(INSERT_CUSTOMER_SQL, customer.getFirstName(), customer.getLastName(), customer.getEmail(),
+                customer.getCompany(), customer.getPhone());
 
-        
-        return null;
+        int id = jdbcTemplate.queryForObject("select Last_INSERT_ID", Integer.class);
+        customer.setCustomerId(id);
+
+        return customer;
     }
 
     @Override
     public List<Customer> getAllCustomers() {
-        return null;
+
+        return jdbcTemplate.query(SELECT_ALL_CUSTOMERS_SQL, this::mapRowToCustomer);
+
     }
 
     @Override
     public Customer getCustomerById(int customerId) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SELECT_CUSTOMER_SQL, this::mapRowToCustomer, customerId);
+        } catch (EmptyResultDataAccessException e) {
+            // if there is no customer with this id, just return null
+            return null;
+        }
     }
 
     @Override
     public void updateCustomer(Customer customer) {
+        jdbcTemplate.update(UPDATE_CUSOTMER_SQL, customer.getFirstName(), customer.getLastName(), customer.getEmail(),
+                customer.getCompany(), customer.getPhone(), customer.getCustomerId());
 
     }
 
     @Override
-    public void deleteCustomer(int id) {
+    public void deleteCustomer(int customerId) {
 
+        jdbcTemplate.update(DELETE_CUSTOMER_SQL, customerId);
+
+    }
+
+    private Customer mapRowToCustomer(ResultSet rs, int rowNum) throws SQLException {
+        Customer customer = new Customer();
+        customer.setFirstName(rs.getString("first_name"));
+        customer.setLastName(rs.getString("last_name"));
+        customer.setCustomerId(rs.getInt("customer_id"));
+        customer.setEmail(rs.getString("email"));
+        customer.setCustomerId(rs.getInt("company"));
+        customer.setPhone(rs.getString("phone"));
+
+        return customer;
     }
 }

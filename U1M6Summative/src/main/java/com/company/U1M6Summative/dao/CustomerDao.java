c87b950eacd4d4e7332bd6389427f9c9f8c0feb3
Your Name <chitramaddy@gmail.com>
2019-07-08 12:03:35 -0400

    Cusotmer Dao and Implementation

--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDao.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDao.java
@@ -1,4 +1,18 @@
 package com.company.U1M6Summative.dao;
 
+import com.company.U1M6Summative.model.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+
+    Customer addCusotmer(Customer customer);
+
+    List<Customer> getAllCustomers();
+
+    Customer getCustomerById(int customerId);
+
+    void updateCustomer(Customer customer);
+
+    void deleteCustomer(int id);
 }
--- U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
+++ U1M6Summative/src/main/java/com/company/U1M6Summative/dao/CustomerDaoJdbcTemplateImpl.java
@@ -1,4 +1,57 @@
 package com.company.U1M6Summative.dao;
 
-public class CustomerDaoJdbcTemplateImpl {
+import com.company.U1M6Summative.model.Customer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CustomerDaoJdbcTemplateImpl implements CustomerDao{
+
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String INSERT_CUSTOMER_SQL =
+            "insert into customer (first_name, last_name, email, company, phone) values (?, ?, ?, ?, ?)";
+
+    private static final String SELECT_CUSTOMER_SQL =
+            "select * from customer where customer_id = ?";
+
+    private static final String SELECT_ALL_CUSTOMERS_SQL =
+            "select * from customer";
+
+    private static final String UPDATE_CUSOTMER_SQL =
+            "update customer set first_name = ?, last_name = ?, email = ?, company = ?, phone = ? where customer_id = ?";
+
+    private static final String DELETE_ALBUM =
+            "delete from customer where customer_id = ?";
+
+
+
+    @Override
+    public Customer addCusotmer(Customer customer) {
+
+        
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Customer getCustomerById(int customerId) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+
+    }
+
+    @Override
+    public void deleteCustomer(int id) {
+
+    }
 }
